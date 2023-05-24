932ccbf24712017d97e6982545517ee21fd03c0b
padmajavadde <vadde.padmaja@gmail.com>
2023-05-24 16:54:48 +0400

    config

--- public/Config.js
+++ public/Config.js
@@ -3,3 +3,18 @@ var decimalDigit=3;
 var DashboardRefreshtime=60000;
 var Dashboarddatetime=100;
 var LiveDataDuration="60000";
+// var FlagCodes=[{Flagcode: "R",FlagName:"estimated"},
+// {Flagcode: "A",FlagName:"correct"},
+// {Flagcode: "O",FlagName:"corrected"},
+// {Flagcode: "P",FlagName:"drift"},
+// {Flagcode: "D",FlagName:"failure"},
+// {Flagcode: "I",FlagName:"invalidated"},
+// {Flagcode: "M",FlagName:"maintenance"},
+// {Flagcode: "N",FlagName:"nonobtained"},
+// {Flagcode: "W",FlagName:"warning"},
+// {Flagcode: "B",FlagName:"anomaly"},
+// {Flagcode: "X",FlagName:"stop"},
+// {Flagcode: "S",FlagName:"substitute"},
+// {Flagcode: "G",FlagName:"outofrange"},
+// {Flagcode: "H",FlagName:"outoffield"}
+// ];
