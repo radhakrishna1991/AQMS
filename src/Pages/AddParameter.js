a828eb03d0ae54b85ac33a738bcf4699fd5477f2
radhakrishna1991 <radhakrishna.g@ispatialtec.com>
2023-07-26 20:34:38 +0530

    for changes

--- src/Pages/AddParameter.js
+++ src/Pages/AddParameter.js
@@ -387,12 +387,12 @@ function AddParameter() {
                 </div>
                 <div className="col-md-12 mb-3">
                   <label for="coefa" className="form-label">COEF A:</label>
-                  <input type="number" className="form-control" id="coefa" placeholder="Enter COEF A" defaultValue="0" required />
+                  <input type="number" className="form-control" id="coefa" placeholder="Enter COEF A" defaultValue="1" required />
                   <div class="invalid-feedback">Please enter COEF A</div>
                 </div>
                 <div className="col-md-12 mb-3">
                   <label for="coefb" className="form-label">COEF B:</label>
-                  <input type="number" className="form-control" id="coefb" placeholder="Enter COEF B" defaultValue="1" required />
+                  <input type="number" className="form-control" id="coefb" placeholder="Enter COEF B" defaultValue="0" required />
                   <div class="invalid-feedback">Please enter COEF B</div>
                 </div>
                 <div className="col-md-12 mb-3">
