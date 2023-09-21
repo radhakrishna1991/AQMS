7a815882e18b07f807866937beaea8ad89d43219
shanmu <shanmukha.vattem@ispatialtec.com>
2023-09-20 22:15:30 -0500

    JWT Token implementation

--- src/utils/CommonFunctions.js
+++ src/utils/CommonFunctions.js
@@ -19,6 +19,50 @@ const CommonFunctions = {
         }      
       }
     },
+    getAuthHeader()
+    {
+      const token = sessionStorage.getItem('Token');
+     // const LoggedInTime = sessionStorage.getItem('LoggedInTime');
+      const tokenExpTime=sessionStorage.getItem('TokenExpTime');
+      var newdate=new Date(tokenExpTime);
+
+     // var expTime=LoggedInTime.set
+      var currentDate=new Date();
+      console.log("currentDate",currentDate);
+      console.log("exp date",tokenExpTime);
+      if(currentDate > newdate)
+      {
+        fetch(CommonFunctions.getWebApiUrl() + "Token", {
+          method: 'POST',
+        }).then((response) => response.json())
+        .then((responseJson) => {
+          if (responseJson) {
+            console.log(responseJson);
+            sessionStorage.setItem("Token",responseJson.token);
+            var currentDate=new Date();
+            currentDate.setMinutes(currentDate.getMinutes()+responseJson.tokenExpirationTime);            
+            sessionStorage.setItem("TokenExpTime",currentDate);
+            console.log(responseJson.token);
+            return { Authorization: 'Bearer ' +responseJson.token ,'app-origin': 'http://localhost:3000'};
+          }
+        })
+      
+      }
+      else{
+        if (token) {
+          return { Authorization: 'Bearer ' +token ,'app-origin': 'http://localhost:3000'};
+        } else {
+          return {};
+        }
+      }
+
+
+     //alert(LoggedInTime);
+    // alert(tokenExpTime);
+
+
+     
+    }
 }
 
 
